83a3cce60b090d48d7fb39f632a95cfd7196a824
ChrisV001 <kvasilovski@yahoo.com>
2025-03-15 18:53:50 +0100

    Added utils, and update pom.xml

---
+++ src/main/java/com/project/soundcheck/utils/Utils.java
@@ -0,0 +1,64 @@
+package com.project.soundcheck.utils;
+
+import com.project.soundcheck.dto.ArticleDTO;
+import com.project.soundcheck.dto.CarModelDTO;
+import com.project.soundcheck.dto.ExhaustSystemDTO;
+import com.project.soundcheck.dto.ReviewDTO;
+import com.project.soundcheck.model.Article;
+import com.project.soundcheck.model.CarModel;
+import com.project.soundcheck.model.ExhaustSystem;
+import com.project.soundcheck.model.Review;
+import java.util.stream.Collectors;
+
+public class Utils {
+
+    public static ArticleDTO mapArticleToArticleDTO(Article article) {
+        ArticleDTO articleDTO = new ArticleDTO();
+
+        articleDTO.setId(article.getId());
+        articleDTO.setContent(article.getContent());
+        articleDTO.setTitle(article.getTitle());
+        articleDTO.setPublishedAt(article.getPublishedAt());
+
+        return articleDTO;
+    }
+
+    public static ReviewDTO mapReviewToReviewDTO(Review review) {
+        ReviewDTO reviewDTO = new ReviewDTO();
+
+        reviewDTO.setId(review.getId());
+        reviewDTO.setCreatedAt(review.getCreatedAt());
+        reviewDTO.setExhaustSystem(review.getExhaustSystem());
+
+        return reviewDTO;
+    }
+
+    public static ExhaustSystemDTO mapExhaustSystemToExhaustSystemDTO(ExhaustSystem exhaustSystem) {
+        ExhaustSystemDTO exhaustSystemDTO = new ExhaustSystemDTO();
+
+        exhaustSystemDTO.setId(exhaustSystem.getId());
+        exhaustSystemDTO.setName(exhaustSystem.getName());
+        exhaustSystemDTO.setReviews(exhaustSystem.getReviews().stream().map(Utils::mapReviewToReviewDTO).collect(Collectors.toList()));
+        exhaustSystemDTO.setType(exhaustSystem.getType());
+        exhaustSystemDTO.setMaterial(exhaustSystem.getMaterial());
+        exhaustSystemDTO.setCarModels(exhaustSystem.getCarModels().stream().map(Utils::mapCarModelToCarModelDTO).collect(Collectors.toSet()));
+        exhaustSystemDTO.setPerformanceMetrics(exhaustSystem.getPerformanceMetrics());
+        exhaustSystemDTO.setSoundProfile(exhaustSystem.getSoundProfile());
+
+        return exhaustSystemDTO;
+    }
+
+    public static CarModelDTO mapCarModelToCarModelDTO(CarModel carModel) {
+        CarModelDTO carModelDTO = new CarModelDTO();
+
+        carModelDTO.setId(carModel.getId());
+        carModelDTO.setModel(carModel.getModel());
+        carModelDTO.setYear(carModel.getYear());
+        carModelDTO.setEngineType(carModel.getEngineType());
+        carModelDTO.setExhaustSystems(
+            carModel.getExhaustSystems().stream().map(Utils::mapExhaustSystemToExhaustSystemDTO).collect(Collectors.toSet())
+        );
+
+        return carModelDTO;
+    }
+}
