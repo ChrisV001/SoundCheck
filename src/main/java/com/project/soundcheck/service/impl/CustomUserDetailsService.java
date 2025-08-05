8517bfcef2b4d3239b1c10feab158183147351f7
ChrisV001 <kvasilovski@yahoo.com>
2025-08-05 22:49:33 +0200

    UserDetailsService update.

--- src/main/java/com/project/soundcheck/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/project/soundcheck/service/impl/CustomUserDetailsService.java
@@ -1,12 +1,15 @@
 package com.project.soundcheck.service.impl;
 
 import com.project.soundcheck.exceptions.CustomException;
+import com.project.soundcheck.model.User;
 import com.project.soundcheck.repo.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -15,8 +18,14 @@ public class CustomUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username)
-                .orElseThrow(() -> new CustomException("Username/Email not found"));
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        Optional<User> userOpt = userRepository.findByUsername(usernameOrEmail);
+
+        if (userOpt.isEmpty()) {
+            userOpt = userRepository.findByEmail(usernameOrEmail);
+        }
+
+        return userOpt
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username or email: " + usernameOrEmail));
     }
 }
